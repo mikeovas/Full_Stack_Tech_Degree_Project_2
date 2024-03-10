45923aae7cfab9d70dfc2be2346c3afd6fea1c4e
mikeovas <mikeovas@gmail.com>
2024-03-10 18:25:38 -0400

    showPage & addPagination functions created

--- js/script.js
+++ js/script.js
@@ -1,30 +1,87 @@
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
+I am aiming for exceeds expectations and only wish to accept that grade.
 */
 
+
+// ELEMENT SELECTORS
+const studentList = document.querySelector(".student-list");
+const paginationList = document.querySelector(".link-list");
+const searchInput = document.querySelector(".studentSearch");
 
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+The `showPage` function:
+/* This function will calculate how many and which students to dynamically show on the current page */
+/* The number of students shown on the page can be changed by adjusting the variable studentsPerPage to the desired number of students to be shown */
 
+// Set the Number of Students to be Shown on the Page
+const studentsPerPage = 9;
 
+function showPage(list, page) {
+   const startIndex = page * studentsPerPage - studentsPerPage;
+   const endIndex = page * studentsPerPage - 1;
+   studentList.innerHTML = "";
+ 
+   for (let i = 0; i < list.length; i++) {
+     if (i >= startIndex && i <= endIndex) {
+       const html = `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture of ${list[i].name.first} ${list[i].name.last}">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>
+       `;
+ 
+       studentList.insertAdjacentHTML("beforeend", html);
+     }
+   }
+ }
+ 
 
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+The`addPagination` function
+/* This function handles calculating how many buttons are needed and dynamically adds them to the page */
 
+function handlePagination(list) {
+   const numberOfButtons = Math.ceil(list.length / studentsPerPage);
+   paginationList.innerHTML = "";
+ 
+   for (let i = 1; i <= numberOfButtons; i++) {
+     const html = `
+       <li>
+         <button type="button">${i}</button>
+       </li>
+     `;
+     paginationList.insertAdjacentHTML("beforeend", html);
+   }
 
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-
+   paginationList.querySelector("button").classList.add("active");
+   
+   /* This event listener handles calling our function below to change the page & add the `active` class  */
+ 
+   paginationList.addEventListener("click", (e) => {
+     const activeButton = paginationList.querySelector(".active");
+     const buttonClicked = e.target.closest("button");
+   
+     if (activeButton && buttonClicked) {
+       activeButton.classList.remove("active");
+     }
+   
+     if (buttonClicked) {
+       buttonClicked.classList.add("active");
+       showPage(list, buttonClicked.innerHTML);
+     }
+   });
+ }
+ 
 
 // Call functions
+showPage(data, 1);
+handlePagination(data);
